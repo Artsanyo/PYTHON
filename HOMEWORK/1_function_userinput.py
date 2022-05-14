6a813707ead979541a0593827e28d9bd0ef1cfd7
Dennis Artsanyo <artsanyod@gmail.com>
2022-05-14 17:43:08 +0300

    1

--- HOMEWORK/1_function_userinput.py
+++ HOMEWORK/1_function_userinput.py
@@ -1,6 +1,9 @@
 # original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 
+from xml.dom.xmlbuilder import _DOMInputSourceStringDataType
+
+
 def reverse_alist (list_x):
     """Scrieti o functie care returneaza reversul unei liste. Lista primita ca parametru nu 
     trebuie modificata."""
@@ -11,3 +14,5 @@ def reverse_alist (list_x):
 
 # print("Original list:", original_list)
 print("Reversed list:", reverse_alist(input("Please type your list: "))) #asign original_list as a parameter to reverse_alist function/call the function
+
+# _DOMInputSourceStringDataType
